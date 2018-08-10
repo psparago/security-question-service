46efec3f08375b34ab397d39f1ab388bb7d28d47
Peter Sparago <peter.sparago@gmail.com>
2018-08-10 10:02:08 -0400

    initial commit

---
+++ src/test/java/org/sparago/security/question/service/api/SecurityQuestionControllerTest.java
@@ -0,0 +1,10 @@
+package org.sparago.security.question.service.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.junit.Assert.*;
+
+public class SecurityQuestionControllerTest {
+
+
+}
